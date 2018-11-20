fd9e83c879f95bdbe059c3047d4cf8967da01fe1
AntonyDamico <--replace-all>
2018-11-20 19:39:00 -0400

    agregando documentacion

--- Habitacion.py
+++ Habitacion.py
@@ -13,36 +13,60 @@ class Habitacion:
 
         if hab_anterior is not None:
             self.hab_anterior = hab_anterior
-
+            # si la habitación anterior no es principal y se cambió la horientación
             if not hab_anterior.principal and self.cambio_orientacion():
-                print('no es la misma')
+                # se agrega una segunda caja a la habitación anterior
                 self.hab_anterior.agregar_caja()
-
+            # agregando caja a la habitación actual
             self.agregar_caja()
 
     def agregar_caja_principal(self, x, y):
+        '''
+        Método para agregar una caja a la habitación principal
+        los argumentos van a ser la posición de esa caja
+        '''
         self.principal = True
         caja_principal = Caja(x, y)
         self.cajas.append(caja_principal)
 
+    '''
+    ========================
+    ||  Métodos privados  ||
+    ========================
+    '''
+
     def agregar_caja(self):
+        '''
+        Agrega un objeto caja nuevo al array de cajas
+        '''
         nueva_caja = self.crear_caja()
         self.cajas.append(nueva_caja)
 
     def crear_caja(self):
+        '''
+        Devuele un nuevo objeto Caja
+        si es la única en la habitación usa el mismo x & y de la anterior
+        si es la segunda, se mueve según sea la orientación anterior
+        '''
+        # última caja de la habitación anterior
         caja_anterior = self.hab_anterior.cajas[-1]
         new_x = caja_anterior.x
         new_y = caja_anterior.y
+
+        # cambiando los x & y de la caja si ya existe una en la habitación
         if self.cajas:
-            # return Caja(new_x, new_y, caja_anterior)
             if self.check_orientacion() == 'horizontal':
                 new_x = caja_anterior.x + self.ancho
             if self.check_orientacion() == 'vertical':
                 new_y = caja_anterior.y + self.alto
+
         return Caja(new_x, new_y, caja_anterior)
-            
 
     def check_orientacion(self):
+        '''
+        Devuelve la orientación con la que venían las cajas
+        puede ser horizontal o vertical
+        '''
         if self.hab_anterior is not None:
             if self.hab_anterior.x != self.x:
                 return 'horizontal'
@@ -52,4 +76,7 @@ class Habitacion:
         return None
 
     def cambio_orientacion(self):
+        '''
+        Devuelve un boolean que indica si la orientación ha cambiado o no
+        '''
         return self.check_orientacion() != self.hab_anterior.check_orientacion()
