fa2b16a2f6bca7790b84db8dea8d5d019a64b954
AntonyDamico <--replace-all>
2018-12-01 23:28:15 -0400

    separando funciones

--- CajaConstructor.py
+++ CajaConstructor.py
@@ -1,32 +1,39 @@
 from Caja import Caja
 
-class CajaConstructor:
 
+class CajaConstructor:
     def __init__(self, habitacion):
         self.habitacion = habitacion
 
     def get_caja(self):
         '''
-        Devuele un nuevo objeto Caja
+        Devuele un nuevo objeto Caja con
+        la posicion adecuada
+        '''
+        caja_anterior = self.habitacion.hab_anterior.cajas[-1]
+        habitacion_nueva_caja = self.habitacion
+        new_x, new_y = self.get_posicion(caja_anterior)
+        return Caja(new_x, new_y, habitacion_nueva_caja, caja_anterior)
+
+    def get_posicion(self, caja_anterior):
+        '''
+        Devuelve la posicion de la caja que va a ser agregada
         si es la única en la habitación usa el mismo x & y de la anterior
         si es la segunda, se mueve según sea la orientación anterior
         '''
-        # última caja de la habitación anterior
-        caja_anterior = self.habitacion.hab_anterior.cajas[-1]
         new_x = caja_anterior.x
         new_y = caja_anterior.y
-        habitacion_nueva_caja = self.habitacion
 
-        # cambiando los x & y de la caja si ya existe una en la habitación
         if self.habitacion.cajas:
             caja_anterior = self.habitacion.cajas[0]
             if self.habitacion.check_orientacion() == 'horizontal':
-                new_x = self.get_nueva_posicion(caja_anterior.x, self.habitacion.ancho)
+                new_x = self.get_nueva_posicion(
+                    caja_anterior.x, self.habitacion.ancho)
             if self.habitacion.check_orientacion() == 'vertical':
-                new_y = self.get_nueva_posicion(caja_anterior.y, self.habitacion.alto)
+                new_y = self.get_nueva_posicion(
+                    caja_anterior.y, self.habitacion.alto)
 
-        return Caja(new_x, new_y, habitacion_nueva_caja, caja_anterior)
-
+        return new_x, new_y
 
     def get_nueva_posicion(self, caja_anterior_pos, distancia):
         '''
@@ -37,4 +44,3 @@ class CajaConstructor:
         if caja_anterior_pos == 0:
             return caja_anterior_pos + distancia
         return 0
-
