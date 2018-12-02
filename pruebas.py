1eb6ffd8a4c624a7acf65243d64c80c9ba44843b
AntonyDamico <--replace-all>
2018-12-01 23:39:05 -0400

    agregando prueba de otras habitaciones

--- pruebas.py
+++ pruebas.py
@@ -1,3 +1,13 @@
 from Habitacion import Habitacion
 from Caja import Caja
+import dibujar
 
+habitaciones = []
+
+habitaciones.append(Habitacion(0, 0, 0, 4, 4))
+habitaciones[0].agregar_caja_principal(0, 4)
+
+habitaciones.append(Habitacion(0,4,0,4,4, habitaciones[0]))
+habitaciones.append(Habitacion(0, 4,4,4,4,habitaciones[1]))
+
+dibujar.dibujar_plano(habitaciones)
