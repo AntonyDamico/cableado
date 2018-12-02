ad1763731cf8a6e2cba99ab64d89ffd8d799729c
AntonyDamico <--replace-all>
2018-12-01 23:34:00 -0400

    quitando codigo viejo

--- Habitacion.py
+++ Habitacion.py
@@ -57,38 +57,6 @@ class Habitacion:
         nueva_caja = constructor_cajas.get_caja()
         self.cajas.append(nueva_caja)
 
-    # def crear_caja(self):
-    #     '''
-    #     Devuele un nuevo objeto Caja
-    #     si es la única en la habitación usa el mismo x & y de la anterior
-    #     si es la segunda, se mueve según sea la orientación anterior
-    #     '''
-    #     # última caja de la habitación anterior
-    #     caja_anterior = self.hab_anterior.cajas[-1]
-    #     new_x = caja_anterior.x
-    #     new_y = caja_anterior.y
-    #     habitacion_nueva_caja = self
-
-    #     # cambiando los x & y de la caja si ya existe una en la habitación
-    #     if self.cajas:
-    #         caja_anterior = self.cajas[0]
-    #         if self.check_orientacion() == 'horizontal':
-    #             new_x = self.get_nueva_posicion(caja_anterior.x, self.ancho)
-    #         if self.check_orientacion() == 'vertical':
-    #             new_y = self.get_nueva_posicion(caja_anterior.y, self.alto)
-
-    #     return Caja(new_x, new_y, habitacion_nueva_caja, caja_anterior)
-
-    # def get_nueva_posicion(self, caja_anterior_pos, distancia):
-    #     '''
-    #     Devuelve una nueva posición para una segunda caja en la habitación
-    #     Si la primera estaba en 0, la empuja hacia la siguiente pared
-    #     Si no los estaba, lo devuelve a la pared que sea 0
-    #     '''
-    #     if caja_anterior_pos == 0:
-    #         return caja_anterior_pos + distancia
-    #     return 0
-
     def check_orientacion(self):
         '''
         Devuelve la orientación con la que venían 
